5b7e0c023e498f859fa17ba00af2ef1ac62b5bcf
edriso <edriso.dev@gmail.com>
2024-06-16 09:45:59 +0300

    add featured jobs and tags sections

---
+++ resources/views/components/section-heading.blade.php
@@ -0,0 +1,5 @@
+<div class="inline-flex items-center gap-x-2">
+    <span class="w-2 h-2 bg-white"></span>
+
+    <h3 class="capitalize font-bold text-xl">{{ $slot }}</h3>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,33 @@
-<x-layout></x-layout>
+<x-layout>
+    <div class="space-y-10">
+        <section>
+            <x-section-heading>Featured jobs</x-section-heading>
+
+            <div class="grid lg:cols-3 gap-8 mt-6">
+                <x-job-card />
+                <x-job-card />
+                <x-job-card />
+            </div>
+        </section>
+
+        <section>
+            <x-section-heading>Tags</x-section-heading>
+
+            <div class="mt-6 space-x-1">
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+            </div>
+        </section>
+
+        <section>
+            <x-section-heading>Recent jobs</x-section-heading>
+        </section>
+    </div>
+</x-layout>
