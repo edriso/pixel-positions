2191bc90ddcb139424531847d5842917d66d2c2a
edriso <edriso.dev@gmail.com>
2024-06-16 08:59:23 +0300

    update config, and add app layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import "./bootstrap";
+
+// We've moved the images to the 'resources' directory instead of 'public'.
+// This change allows Vite to handle the images, providing benefits like versioning and cache busting.
+// Utilizing 'import.meta.glob' ensures that all images in the specified path are processed efficiently.
+import.meta.glob(["../images/*"]);
