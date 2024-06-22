d91d134ce4dd244333b0af9c1999f17e5a385f56
edriso <edriso.dev@gmail.com>
2024-06-22 22:55:30 +0300

    fix logo display

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,4 +1,8 @@
 @props(['employer', 'width' => 100])
 
-<img src='{{ asset($employer->logo) }}' alt='{{ $employer->name }}' class='rounded-xl' width='{{ $width }}'>
+@php
+    $logo = filter_var($employer->logo, FILTER_VALIDATE_URL) ? $employer->logo : asset('storage/' . $employer->logo);
+@endphp
+
+<img src="{{ $logo }}" alt='{{ $employer->name }}' class='rounded-xl' width='{{ $width }}' />
 {{-- <img src="http://picsum.photos/seed/{{ rand(0, 1000) }}/{{ $width }}" alt="Employer's logo" class="rounded-xl" /> --}}
